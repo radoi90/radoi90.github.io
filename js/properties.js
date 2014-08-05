02815d8bad3a969df690bca275d22f1dd0aa9010
Emil <eculic17@gmail.com>
2014-08-05 14:16:43 +0100

    Revert "Revert c99fba8..2587c7e"
    This reverts commit 7ee248973501dc6bfed7470fdb0346a77dbc54e7.

--- js/properties.js
+++ js/properties.js
@@ -1,4 +1,6 @@
-var dateDiff = function(date) {
+var dateDiff = function(s) {
+  var a = s.split(/[^0-9]/);
+  var date = new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5] );
   var milisDiff = new Date() - date, 
         secDiff = milisDiff / 1000,
         minDiff = secDiff / 60,
@@ -18,27 +20,58 @@ var dateDiff = function(date) {
   else return "moments ago";
 }
 
-var map;
+var getJSONP = function(url, success) {
+  var ud = '_' + +new Date,
+      script = document.createElement('script'),
+      head = document.getElementsByTagName('head')[0] 
+             || document.documentElement;
 
-function initializeMap() {
+  window[ud] = function(data) {
+      head.removeChild(script);
+      success && success(data);
+  };
+
+  script.src = url.replace('callback=?', 'jsonp=' + ud);
+  head.appendChild(script);
+};
+
+var map, inputProperty, hook;
+
+var initializeMap = function() {
   var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(51.53, -0.1)
+    zoom: 11,
+    center: new google.maps.LatLng(51.5072, -0.1275)
   }
   
   map = new google.maps.Map(document.getElementById('map-canvas'),
                                 mapOptions);
-}
+  var transitLayer = new google.maps.TransitLayer();
+  transitLayer.setMap(map);
+};
 
-function setMarker(map, property) {
+var setMarker = function(view) {
+  var property =  view.model;
   var myLatLng = new google.maps.LatLng(property.get('content').latitude, property.get('content').longitude);
+
+  var image = {
+    url: 'https://embed-dot-more-than-a-map.appspot.com/images/marker-' 
+    + String.fromCharCode(65 + $("#"+ view.el.id).index()) +'.png',
+    // This marker is 20 pixels wide by 32 pixels tall.
+    size: new google.maps.Size(48, 63),
+    // The origin for this image is 0,0.
+    origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the flagpole at 0,32.
+    anchor: new google.maps.Point(23, 63)
+  };
+
   var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: property.get('order') + "",
-      zIndex: property.get('order')
+    position: myLatLng,
+    map: map,
+    icon: image,
+    title: "1",
+    zIndex: -$("#"+ view.el.id).index()
   });
-}
+};
 
 $(function() {
 
@@ -126,7 +159,10 @@ $(function() {
 
     // Properties are sorted by their listing date.
     comparator: function(property) {
-      return -Date.parse(property.get('content').last_published_date);
+      var s = property.get('content').last_published_date;
+      var a = s.split(/[^0-9]/);
+      var date = new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5] );
+      return -date;
     }
 
   });
@@ -145,19 +181,20 @@ $(function() {
 
     // The DOM events specific to an item.
     events: {
-      "click .markview"              : "toggleViewed",
+      "click .markview"          : "toggleViewed",
       "click .star"              : "toggleStar",
       "click .hide"              : "toggleHidden",
-      "click .property-destroy"   : "clear",
+      "click .property-destroy"  : "clear",
+      "mouseleave .listing"      : "highlight"  
     },
 
     // The PropertyView listens for changes to its model, re-rendering. Since there's
     // a one-to-one correspondence between a Property and a PropertyView in this
     // app, we set a direct reference on the model for convenience.
     initialize: function() {
-      _.bindAll(this, 'render', 'close', 'remove');
-      this.model.bind('change', this.render);
+      _.bindAll(this, 'render', 'remove');
       this.model.bind('destroy', this.remove);
+      this.el.id = this.model.get("content").listing_id;
     },
 
     // Re-render the contents of the property item.
@@ -184,6 +221,10 @@ $(function() {
     // Remove the item, destroy the model.
     clear: function() {
       this.model.destroy();
+    },
+
+    highlight: function() {
+      console.log("hover");
     }
 
   });
@@ -200,8 +241,6 @@ $(function() {
     // Delegated events for creating new items, and clearing completed ones.
     events: {
       "keypress #new-property":  "createOnEnter",
-      "click #clear-hidden": "clearHidden",
-      "click #view-all": "toggleAllViewed",
       "click .log-out": "logOut",
       "click ul#filters a": "selectFilter"
     },
@@ -214,13 +253,12 @@ $(function() {
     initialize: function() {
       var self = this;
 
-      _.bindAll(this, 'addOne', 'addAll', 'addActive', 'addSome', 'render', 'toggleAllViewed', 'logOut', 'createOnEnter');
+      _.bindAll(this, 'addOne', 'addAll', 'addActive', 'addSome', 'render', 'logOut', 'createOnEnter');
 
       // Main property management template
       this.$el.html(_.template($("#manage-properties-template").html()));
       
       this.input = this.$("#new-property");
-      this.allViewIcon = this.$("#view-all")[0];
 
       // Create our collection of Properties
       this.properties = new PropertyList;
@@ -262,13 +300,11 @@ $(function() {
       }));
 
       this.delegateEvents();
-
-      this.allViewIcon.checked = viewed;
     },
 
     // Filters the list based on which type of filter is selected
     selectFilter: function(e) {
-      var el = $(e.target);
+      var el = $(e.target.parentElement);
       var filterValue = el.attr("id");
       state.set({filter: filterValue});
       Parse.history.navigate(filterValue);
@@ -276,8 +312,8 @@ $(function() {
 
     filter: function() {
       var filterValue = state.get("filter");
-      this.$("ul#filters a").removeClass("selected");
-      this.$("ul#filters a#" + filterValue).addClass("selected");
+      this.$("ul#filters li").removeClass("selected");
+      this.$("ul#filters li#" + filterValue).addClass("selected");
       if (filterValue === "all") {
         this.addAll();
       } else if (filterValue === "starred") {
@@ -293,8 +329,8 @@ $(function() {
 
     // Resets the filters to display all properties
     resetFilters: function() {
-      this.$("ul#filters a").removeClass("selected");
-      this.$("ul#filters a#active").addClass("selected");
+      this.$("ul#filters li").removeClass("selected");
+      this.$("ul#filters li#active").addClass("selected");
       this.addSome(function(item) { return !item.get('hidden') });
     },
 
@@ -303,7 +339,10 @@ $(function() {
     addOne: function(property) {
       var view = new PropertyView({model: property});
       this.$("#property-list").append(view.render().el);
-      setMarker(map, property);
+      $("#" + view.el.id + " h4").text(
+        String.fromCharCode(65 + $("#"+ view.el.id).index()) +". " +$("#" + view.el.id + " h4").text());
+      
+      setMarker(view);
     },
 
      // Add all items in the Properties collection at once.
@@ -330,38 +369,33 @@ $(function() {
 
     // If you hit return in the main input field, create new Property model
     createOnEnter: function(e) {
-      var self = this;
+      hook = this;
       if (e.keyCode != 13) return;
+      var zooplaAPI = 'http://api.zoopla.co.uk/api/v1/property_listings.js?listing_id=' +
+                      this.input.val() +
+                      '&api_key=kwt27yfdcvd6ek4gq2bqy2z5&callback=?';
+      this.input.val('');
+      getJSONP(zooplaAPI, function(data) {
+        hook.properties.create({
+          //TODO:
+          content:         data.listing[0],
+          hidden:          false,
+          starred:         false,
+          viewed:          false,
+          user:            Parse.User.current(),
+          ACL:             new Parse.ACL(Parse.User.current())
+        });
 
-      this.properties.create({
-        //TODO:
-        content:         JSON.parse(this.input.val()),
-        hidden:          false,
-        starred:         false,
-        viewed:          false,
-        user:            Parse.User.current(),
-        ACL:             new Parse.ACL(Parse.User.current())
+        hook.resetFilters();
       });
-
-      this.resetFilters();
-    },
-
-    // Clear all hidden property items, destroying their models.
-    clearHidden: function() {
-      _.each(this.properties.hidden(), function(property){ property.destroy(); });
-      return false;
-    },
-
-    toggleAllViewed: function () {
-      var viewed = this.allViewIcon.checked;
-      this.properties.each(function (property) { property.save({'viewed': viewed}); });
     }
   });
 
   var LogInView = Parse.View.extend({
     events: {
       "submit form.login-form": "logIn",
-      "submit form.signup-form": "signUp"
+      "submit form.signup-form": "signUp",
+      "click a.reset-password": "resetPassword"
     },
 
     el: ".content",
@@ -384,7 +418,8 @@ $(function() {
         },
 
         error: function(user, error) {
-          self.$(".login-form .error").html("Invalid email or password. Please try again.").show();
+          self.$(".login-form .success").hide();  
+          self.$(".login-form .error").html("Invalid email or password. Please try again. Or, <a class='reset-password'>reset password</a>.").show();
           self.$(".login-form button").removeAttr("disabled");
         }
       });
@@ -420,6 +455,26 @@ $(function() {
       this.$(".signup-form button").attr("disabled", "disabled");
 
       return false;
+    },
+
+    resetPassword: function() {
+      var email = this.$("#login-email").val();
+
+      Parse.User.requestPasswordReset(email, {
+        success: function() {
+          self.$(".login-form .success").html("We've sent you a password reset email to " + email).show();
+          self.$(".login-form .error").hide();
+        },
+        error: function(error) {
+          self.$(".login-form .success").hide();
+          if(email.length > 0) {
+            self.$(".login-form .error").html("There is no user assigned to " + email).show();
+          } else {
+            self.$(".login-form .error").html("To reset your password enter the email you used to sign up.").show();
+          }          
+          self.$(".login-form button").removeAttr("disabled");
+        }
+      });
     },
 
     render: function() {
